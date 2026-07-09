61833b08030a45c2a95ffc7a88607f47f94b41e9
Minhch207 <haminhchau83@gmail.com>
2026-07-09 11:35:01 +0700

    Add files via upload

---
+++ config/fish/config.fish
@@ -0,0 +1,9 @@
+#source /usr/share/cachyos-fish-config/cachyos-config.fish
+export EDITOR=nvim
+export FLAG=FromMIXIwithLove
+# overwrite greeting
+# potentially disabling fastfetch
+function fish_greeting
+    #fastfetch
+    #    # smth smth
+end
